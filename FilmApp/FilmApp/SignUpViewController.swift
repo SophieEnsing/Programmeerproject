192d2b8c80d0928526e8b2a6e0475d74e673ff63
Sophie Ensing <sophie_ensing@hotmail.com>
2018-01-15 11:07:30 +0100

    15/01

---
+++ FilmApp/FilmApp/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  FilmApp
+//
+//  Created by Sophie Ensing on 15-01-18.
+//  Copyright © 2018 Sophie Ensing. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
