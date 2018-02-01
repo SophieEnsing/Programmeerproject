deeb4530ad77ecb6d16bf717e7d4803c9ef6af09
Sophie Ensing <sophie_ensing@hotmail.com>
2018-02-01 15:34:42 +0100

    Meer comments

--- FilmApp/FilmApp/AppDelegate.swift
+++ FilmApp/FilmApp/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
         -> Bool {
+            // Make the Firebase connection.
             FirebaseApp.configure()
+            
+            // Apply styling to the navigation and status bar.
             UINavigationBar.appearance().barStyle = .blackOpaque
             UIApplication.shared.statusBarStyle = .lightContent
             return true
